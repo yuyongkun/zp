59b12b4bfc66b02d2a14913643d1c4be3ed4b83d
yuyongkun <yuyongkun4519@qq.com>
2017-03-30 22:19:50 +0800

    添加后台

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/home/index');
-var users = require('./routes/home/users');
+var admin = require('./routes/admin/admin');
 
 var app = express();
 // view engine setup
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
-app.use('/users', users);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
