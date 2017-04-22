c505e72bf30fb80131670f2a87bce2ba4daa5f82
yuyongkun <yuyongkun4519@qq.com>
2017-04-22 23:28:16 +0800

    去掉默认ip

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.set('port', 3003);
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
