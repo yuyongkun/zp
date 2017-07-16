977f8c8bd7b88181e4fef1af7c98e32f5c81719d
lusen <1265852100@qq.com>
2017-07-16 15:55:03 +0800

    1111111111111

---
+++ public/static/admin/js/paginationIndex.js
@@ -0,0 +1,40 @@
+function  pagehtml(pagehelp){
+	var href=pagehelp.href;
+	var pagecount = pagehelp.pagecount;
+	var pagesize = pagehelp.pagesize;
+	var currentpage = pagehelp.currentpage;
+	var counts,pagehtml="";
+	if(pagecount%pagesize==0){
+	    counts = parseInt(pagecount/pagesize);
+	}else{
+	    counts = parseInt(pagecount/pagesize)+1;
+	}
+	// 大于一页内容
+		if(currentpage>0){
+			var num;
+			if(currentpage==1){
+				num=parseInt(currentpage);
+			}else{
+				num=parseInt(currentpage)-parseInt(1);
+			}
+		   pagehtml+= '<li><a class="previous" href="'+href+'/'+(num)+'">上一页</a></li>';
+		}
+		for(var i=1;i<=counts;i++){
+			if(i==currentpage){
+			   pagehtml+= '<li><a class="current"  href="'+href+'/'+i+'">'+i+'</a></li>';
+			}else{
+			   pagehtml+= '<li><a href="'+href+'/'+i+'">'+i+'</a></li>';
+			}
+		}
+		if(currentpage<counts+1){
+			var num;
+			if(currentpage==counts){
+				num=parseInt(currentpage);
+			}else{
+				num=parseInt(currentpage)+parseInt(1);
+			}
+		  pagehtml+= '<li><a class="next" href="'+href+'/'+(num)+'">下一页</a></li>';
+		}
+	return pagehtml;
+}
+exports.pagehtml  = pagehtml;
